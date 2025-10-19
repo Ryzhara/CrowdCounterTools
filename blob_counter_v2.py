aceca7eeae33181cb02e6b955a5bc66d63cdfcfd
Robin Fredericksen <refredericksen@gmail.com>
2025-10-19 14:00:43 -0500

    Add additional "primary" colors for marking

--- blob_counter_v2.py
+++ blob_counter_v2.py
@@ -18,6 +18,9 @@ NAMED_COLORS: Final[dict[str, RgbColor]] = {
     "red": (255, 0, 0),
     "green": (0, 255, 0),
     "blue": (0, 0, 255),
+    "yellow": (255, 255, 0),
+    "purple": (255, 0, 255),
+    "cyan": (0, 255, 255),
 }
 
 
